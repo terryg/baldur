66e9eb8f1baeb078ec8d17988c2fb1ca377a219d
Terry Lorber <terry@metatooth.com>
2019-06-11 10:02:55 -0400

    Simplify

---
+++ models/settings.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Name-value pairs to configure things.
+class Settings
+  include DataMapper::Resource
+
+  property :id, Serial
+  property :name, String
+  property :value, String
+
+end
